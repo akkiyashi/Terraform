bf0fd2b5345ffb1939267340c1f21658abddc8a0
akkiyashi <30432829+akkiyashi@users.noreply.github.com>
2020-01-14 13:08:46 +0530

    Update instance.tf

--- Modules/ec2/instance.tf
+++ Modules/ec2/instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "webserver"{
-	count = "${var.count}"
+	count = "${var.countno}"
 	ami = "${var.amiid}"
-	instance_type = "${var.type}"
+	instance_type = "${var.i_type}"
 	subnet_id = "${var.subnet_id}"
 	tags = {
 	Name = "Webserver"
